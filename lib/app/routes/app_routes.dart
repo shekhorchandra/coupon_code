0d68b1a05d172d3a293027b36ea7cd1334f9a72c
Betopia <shekhorchandrasah@gmail.com>
2026-01-28 16:43:53 +0600

    initial

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -12,11 +12,17 @@ abstract class AppRoutes {
   static const VENDOR_FORGOT_PASSWORD = '/vendor/forgot-password';
 
   static const DISCOVER = '/discover';
-  static const CATEGORIES = '/discover';
-  static const SAVED = '/discover';
+  static const DISCOVERDETAILS = '/discover-details';
+  static const CATEGORIES = '/category';
+  static const CATEGORIESDETAILS = '/category-details';
+  static const SAVED = '/save';
   static const MENU = '/menu';
 
   static const ABOUT = '/about';
+  static const CONTACT_US = '/contact-us';
+  static const HELP_SUPPORT = '/help-support';
+  static const TERMSCONDITION = '/terms-condition';
+  static const PRIVACYPOLICY = '/privacy-policy';
 
   // Vendor Part
   static const CREATE_VENDOR_ACCOUNT = '/vendor/account/create';
