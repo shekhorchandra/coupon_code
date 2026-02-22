4ce5f950a62fbc766bfffa3014fcfa944ff07191
Tonoy Saha (Shekhor) <shekhorsaha058@gmail.com>
2026-02-22 13:22:36 +0600

    add vendor register end point

--- lib/app/modules/services/contants/api_constants.dart
+++ lib/app/modules/services/contants/api_constants.dart
@@ -1,10 +1,11 @@
 class ApiConstants {
-  static const String baseUrl = "https://gastrotomic-squirrelly-yuonne.ngrok-free.dev/api/v1";
+  static const String baseUrl =
+      "https://gastrotomic-squirrelly-yuonne.ngrok-free.dev/api/v1";
 
   // Define endpoints
-  static const String login = "/auth/login";
-// static const String login = "/auth/login";
-// static const String register = "/auth/register";
-// static const String getProfile = "/user/profile";
-// Add more endpoints as needed
+  static const String vendorLogin = "/auth/login";
+  static const String vendorRegister = "/user/register";
+  // static const String register = "/auth/register";
+  // static const String getProfile = "/user/profile";
+  // Add more endpoints as needed
 }
