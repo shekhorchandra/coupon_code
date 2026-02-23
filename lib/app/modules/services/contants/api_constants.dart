63bc4143de68b6a348ad190bfa0a860482181b31
Tonoy Saha (Shekhor) <shekhorsaha058@gmail.com>
2026-02-23 12:41:51 +0600

    add endpoints for forget password, otp verification, reset password

--- lib/app/modules/services/contants/api_constants.dart
+++ lib/app/modules/services/contants/api_constants.dart
@@ -2,6 +2,13 @@ class ApiConstants {
   static const String baseUrl = "https://gastrotomic-squirrelly-yuonne.ngrok-free.dev/api/v1";
 
   // Define endpoints
+
+  // Auth endpoints
+  static const String forgetPassword = "/auth/forget_password/";
+  static const String verifyOtp = "/auth/verify_otp";
+  static const String resetPassword = "/auth/reset_password";
+
+  // vendor login and signup
   static const String vendorLogin = "/auth/login";
   static const String vendorRegister = "/user/register";
   // static const String register = "/auth/register";
