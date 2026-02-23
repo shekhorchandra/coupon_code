3c08eb98275420a4dab9af9800daae95c7cea348
Avizit Roy <contact@avizitrx.com>
2026-02-23 10:59:27 +0600

    chore: firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:coupon_code/app/core/theme/checkbox_theme.dart';
+import 'package:coupon_code/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
 import 'app/core/values/app_strings.dart';
 import 'app/routes/app_pages.dart';
 import 'app/routes/app_routes.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const CouponDiscountApp());
 }
 
