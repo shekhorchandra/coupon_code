a0af9c13164889e52a9b2ffa24fcaca729ee8ba5
Avizit Roy <contact@avizitrx.com>
2026-02-24 12:28:45 +0600

    feat: logout

--- lib/app/modules/vendor/vendor_menu/controllers/vendor_menu_controller.dart
+++ lib/app/modules/vendor/vendor_menu/controllers/vendor_menu_controller.dart
@@ -1,9 +1,36 @@
+import 'package:coupon_code/app/data/network/dio_client.dart';
+import 'package:coupon_code/app/data/services/storage_service.dart';
+import 'package:coupon_code/app/modules/services/contants/api_constants.dart';
+import 'package:coupon_code/app/routes/app_routes.dart';
 import 'package:get/get.dart';
 
 class VendorMenuController extends GetxController {
   VendorMenuController();
 
-  final _obj = ''.obs;
-  set obj(value) => _obj.value = value;
-  String get obj => _obj.value;
+  RxBool loading = false.obs;
+
+  final DioClient _dioClient = DioClient();
+  final StorageService _storageService = StorageService();
+
+  Future<void> logout() async {
+    loading.value = true;
+
+    try {
+      // Remove tokens
+      await _storageService.clear();
+
+      // Remove FCM
+      final response = await _dioClient.client.post(ApiConstants.fcmUnregister);
+
+      if (response.statusCode == 200) {
+        Get.toNamed(AppRoutes.USER_BOTTOM_NAV);
+      } else {
+        Get.snackbar('Error', 'Failed to log out!');
+      }
+    } catch (e) {
+      Get.snackbar('Error', 'An unexpected error occured: $e');
+    } finally {
+      loading.value = false;
+    }
+  }
 }
--- lib/app/modules/vendor/vendor_menu/views/vendor_menu_view.dart
+++ lib/app/modules/vendor/vendor_menu/views/vendor_menu_view.dart
@@ -3,6 +3,7 @@ import 'package:coupon_code/app/core/widgets/App_button.dart';
 import 'package:coupon_code/app/core/widgets/common_app_bar.dart';
 import 'package:coupon_code/app/core/widgets/section_heading.dart';
 import 'package:coupon_code/app/modules/vendor/vendor_account/controllers/vendor_account_controller.dart';
+import 'package:coupon_code/app/modules/vendor/vendor_menu/controllers/vendor_menu_controller.dart';
 import 'package:coupon_code/app/modules/vendor/vendor_menu/views/widgets/menu_item.dart';
 import 'package:coupon_code/app/modules/vendor/vendor_menu/views/widgets/profile_overview_card.dart';
 import 'package:coupon_code/app/routes/app_routes.dart';
@@ -14,67 +15,75 @@ class VendorMenuPage extends GetView<VendorAccountController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CommonAppBar(title: 'Menu', showBack: false),
+    final menuController = Get.put(VendorMenuController());
 
-      body: SafeArea(
-        child: SingleChildScrollView(
-          padding: EdgeInsets.all(AppSizes.screenPadding),
-          child: Column(
-            children: [
-              // User Info
-              GestureDetector(onTap: () {}, child: ProfileOverviewCard()),
-              const SizedBox(height: 20),
+    return Obx(() {
+      if (menuController.loading.value) {
+        return const Scaffold(body: Center(child: CircularProgressIndicator()));
+      }
 
-              // Settings
-              SectionHeading(title: 'Settings'),
+      return Scaffold(
+        appBar: CommonAppBar(title: 'Menu', showBack: false),
 
-              KMenuItem(
-                title: 'Payment Methods',
-                icon: Icons.credit_card_rounded,
-                onTap: () => Get.toNamed(AppRoutes.PAYMENT_METHOD),
-              ),
-              KMenuItem(
-                title: 'About Us',
-                icon: Icons.info_outline_rounded,
-                onTap: () => Get.toNamed(AppRoutes.ABOUT),
-              ),
-              KMenuItem(
-                title: 'Contact Us',
-                icon: Icons.headphones_outlined,
-                onTap: () => Get.toNamed(AppRoutes.CONTACT_US),
-              ),
-              KMenuItem(
-                title: 'Help & Support',
-                icon: Icons.help_outline_rounded,
-                onTap: () => Get.toNamed(AppRoutes.HELP_SUPPORT),
-              ),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(AppSizes.screenPadding),
+            child: Column(
+              children: [
+                // User Info
+                GestureDetector(onTap: () {}, child: ProfileOverviewCard()),
+                const SizedBox(height: 20),
 
-              KMenuItem(
-                title: 'Privacy Policy',
-                icon: Icons.privacy_tip_outlined,
-                onTap: () => Get.toNamed(AppRoutes.PRIVACYPOLICY),
-              ),
-              KMenuItem(
-                title: 'Terms & Conditions',
-                icon: Icons.description_outlined,
-                onTap: () => Get.toNamed(AppRoutes.TERMSCONDITION),
-              ),
-              KMenuItem(title: 'Rate the App', icon: Icons.star_rate_rounded, onTap: () {}),
-              KMenuItem(
-                title: 'Invite Friends',
-                icon: Icons.share_outlined,
-                onTap: () {},
-                showDivider: false,
-              ),
+                // Settings
+                SectionHeading(title: 'Settings'),
 
-              // Logout
-              const SizedBox(height: 20),
-              AppButton(text: 'Logout', onPressed: () => Get.toNamed(AppRoutes.USER_BOTTOM_NAV)),
-            ],
+                KMenuItem(
+                  title: 'Payment Methods',
+                  icon: Icons.credit_card_rounded,
+                  onTap: () => Get.toNamed(AppRoutes.PAYMENT_METHOD),
+                ),
+                KMenuItem(
+                  title: 'About Us',
+                  icon: Icons.info_outline_rounded,
+                  onTap: () => Get.toNamed(AppRoutes.ABOUT),
+                ),
+                KMenuItem(
+                  title: 'Contact Us',
+                  icon: Icons.headphones_outlined,
+                  onTap: () => Get.toNamed(AppRoutes.CONTACT_US),
+                ),
+                KMenuItem(
+                  title: 'Help & Support',
+                  icon: Icons.help_outline_rounded,
+                  onTap: () => Get.toNamed(AppRoutes.HELP_SUPPORT),
+                ),
+
+                KMenuItem(
+                  title: 'Privacy Policy',
+                  icon: Icons.privacy_tip_outlined,
+                  onTap: () => Get.toNamed(AppRoutes.PRIVACYPOLICY),
+                ),
+                KMenuItem(
+                  title: 'Terms & Conditions',
+                  icon: Icons.description_outlined,
+                  onTap: () => Get.toNamed(AppRoutes.TERMSCONDITION),
+                ),
+                KMenuItem(title: 'Rate the App', icon: Icons.star_rate_rounded, onTap: () {}),
+                KMenuItem(
+                  title: 'Invite Friends',
+                  icon: Icons.share_outlined,
+                  onTap: () {},
+                  showDivider: false,
+                ),
+
+                // Logout
+                const SizedBox(height: 20),
+                AppButton(text: 'Logout', onPressed: menuController.logout),
+              ],
+            ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
