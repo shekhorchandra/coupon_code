f7516e1b4777e5bd2508a36bfeb834bb0f908dcc
Avizit Roy <contact@avizitrx.com>
2026-03-17 12:07:19 +0600

    fix: controller issue on deals page

--- lib/app/modules/vendor/vendor_deals/views/widgets/vendor_deals_list.dart
+++ lib/app/modules/vendor/vendor_deals/views/widgets/vendor_deals_list.dart
@@ -10,7 +10,7 @@ class VendorDealsList extends GetView<VendorDashboardController> {
 
   @override
   Widget build(BuildContext context) {
-    final dealController = Get.find<VendorDealsController>();
+    final dealController = Get.put(VendorDealsController());
 
     return Obx(() {
       if (controller.isLoading.value) {
