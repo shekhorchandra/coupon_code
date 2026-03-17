314e82c947d515fd3285974e95bba6cb30da6c5f
Avizit Roy <contact@avizitrx.com>
2026-03-17 12:07:19 +0600

    fix: controller issue on deals page

--- lib/app/modules/vendor/vendor_deals/bindings/vendor_deals_binding.dart
+++ lib/app/modules/vendor/vendor_deals/bindings/vendor_deals_binding.dart
@@ -1,9 +1,11 @@
+import 'package:coupon_code/app/modules/vendor/vendor_dashboard/controllers/vendor_dashboard_controller.dart';
 import 'package:coupon_code/app/modules/vendor/vendor_deals/controllers/vendor_deals_controller.dart';
 import 'package:get/get.dart';
 
 class VendorDealsBinding implements Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<VendorDashboardController>(() => VendorDashboardController());
     Get.lazyPut<VendorDealsController>(() => VendorDealsController());
   }
 }
