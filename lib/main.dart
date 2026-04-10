23783b882d955df3486bb3bc46bbaad4e39ee474
Avizit Roy <contact@avizitrx.com>
2026-04-10 11:16:29 +0600

    chore: .env setup

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:coupon_code/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 
 import 'app/core/values/app_strings.dart';
@@ -14,6 +15,9 @@ import 'app/routes/app_routes.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Load .env
+  await dotenv.load(fileName: ".env");
 
   // Firebase initialization
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
